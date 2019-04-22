1d331df05dd2862bd7550cafd6f2fd4458f080ca
Vincent <vincent.gbs11@gmail.com>
2019-04-22 08:45:39 -0400

    Moved helper files and functions into separate folders, added extra columns to structure

--- rules_based/main.c
+++ rules_based/main.c
@@ -1,5 +1,5 @@
-#include "dictionary.h"
-#include "parser.h"
+#include "src/dictionary.h"
+// #include "parser.h"
 
 int main() {
     int i, j; // iterators
@@ -8,7 +8,7 @@ int main() {
     char line[512];
     int max = 0; // max lines (only for testing purposes)
 
-    while (fgets(line, 512, stream) && (max < 999))
+    while (fgets(line, 512, stream) && (max < 9999))
     {
         char* str = strdup(line); // duplicate line as string
         char *p = strtok(str, ","); // split line on comas
@@ -20,7 +20,8 @@ int main() {
             p = strtok(NULL, ",");
         }
 
-        add_record(row[0], atoi(row[7]), atoi(row[8]), atoi(row[9]));
+        add_record(row[0], atoi(row[7]), atoi(row[8]), atoi(row[9]),
+            atoi(row[10]), atoi(row[11]), atoi(row[12]), atoi(row[13]), atoi(row[14]));
 
         free(str); // deallocate memory from line pointer
         max++; // only for testing
--- rules_based/src/dictionary.h
+++ rules_based/src/dictionary.h
@@ -4,7 +4,7 @@
 #include <stdio.h>   /* gets */
 #include <stdlib.h>  /* atoi, malloc */
 #include <string.h>  /* strcpy */
-#include "uthash.h"
+#include "../library/uthash.h"
 
 #define WORDLEN 50
 // #define DICTLEN 86486 // 86487 lines with header
@@ -14,17 +14,18 @@ struct my_struct {
     int Negative; // [7]
     int Positive; // [8]
     int Uncertainty; // [9]
-    // int Litigious; // 10
-    // int Constraining; // 11
-    // int Superfluous; // 12
-    // int Interesting; // 13
-    // int Modal; // 14
+    int Litigious; // [10]
+    int Constraining; // [11]
+    int Superfluous; // [12]
+    int Interesting; // [13]
+    int Modal; // [14]
     UT_hash_handle hh; /* makes this structure hashable */
 };
 
 struct my_struct *dictionary = NULL;
 
-void add_record(char *word, int neg, int pos, int uncert) {
+void add_record(char *word, int neg, int pos, int uncert,
+    int litig, int constrain, int superf, int interest, int modal) {
     struct my_struct *s;
 
     HASH_FIND_INT(dictionary, word, s);  /* id already in the hash? */
@@ -34,6 +35,11 @@ void add_record(char *word, int neg, int pos, int uncert) {
         s->Negative = neg;
         s->Positive = pos;
         s->Uncertainty = uncert;
+        s->Litigious = litig;
+        s->Constraining = constrain;
+        s->Superfluous = superf;
+        s->Interesting = interest;
+        s->Modal = modal;
         HASH_ADD_INT( dictionary, id, s );  /* id: name of key field */
     }
 }
@@ -49,7 +55,8 @@ void print_dictionary() {
     struct my_struct *s;
 
     for(s=dictionary; s != NULL; s=(struct my_struct*)(s->hh.next)) {
-        printf("word %s: pos %d neg %d uncert %d\n", s->id, s->Positive, s->Negative, s->Uncertainty);
+        printf("word %s: pos %d, neg %d, uncert %d, lit %d, mod %d \n",
+            s->id, s->Positive, s->Negative, s->Uncertainty, s->Litigious, s->Modal);
     }
 }
 
