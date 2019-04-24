be96d04fabe60b3d173a47e21cbbe8ad6ec3e039
Vincent <vincent.gbs11@gmail.com>
2019-04-24 17:07:47 -0400

    Trying to figure out why decreasing inputs can cause a segmentation error

--- learn_regression/main.c
+++ learn_regression/main.c
@@ -8,17 +8,19 @@
 #define TRAIN
 
 int main () {
-  char line[1024];
-  getReviewsb("7.txt", 200);
-  initializePointArray(pointArray, 500);
-  // for (int i = 0; i < 500; i++) {
-  //     printf("%d: %f - %d - %f - %f\n", i, pointArray[i].avgPoint, pointArray[i].frequency, pointArray[i].actualRate, pointArray[i].predictRate);
-  // }
-  fileToArray("7.txt", 200);
-  actualRateRegression(pointArray, 500);
-  for (int i = 0; i < 500; i++) {
-      printf("%d: %f - %d - %f - %f\n", i, pointArray[i].avgPoint, pointArray[i].frequency, pointArray[i].actualRate, pointArray[i].predictRate);
-  }
-  predictAllReview ("7.txt", 700);
-  return 0;
+    char line[1024];
+    getReviewsb("7.txt", 200);
+    // printList(wordNodeArray);
+    initializePointArray(pointArray, 500);
+    // for (int i = 0; i < 500; i++) {
+    //     printf("%d: %f - %d - %f - %f\n", i, pointArray[i].avgPoint, pointArray[i].frequency, pointArray[i].actualRate, pointArray[i].predictRate);
+    // }
+    fileToArray("7.txt", 200);
+    actualRateRegression(pointArray, 500);
+    // for (int i = 0; i < 500; i++) {
+    //     printf("%d: %f - %d - %f - %f\n", i, pointArray[i].avgPoint, pointArray[i].frequency, pointArray[i].actualRate, pointArray[i].predictRate);
+    // }
+    // printList(wordNodeArray);
+    predictAllReview ("7.txt", 700);
+    return 0;
 }
