355e9f83000bfe90f76f455c03fdeaa111e98007
Vincent <vincent.gbs11@gmail.com>
2019-04-24 16:42:36 -0400

    Working to clean up regression code

--- learn_regression/main.c
+++ learn_regression/main.c
@@ -7,13 +7,18 @@
 
 #define TRAIN
 
-
 int main () {
   char line[1024];
-  getReviewsb("7.txt",2000);
+  getReviewsb("7.txt", 200);
   initializePointArray(pointArray, 500);
-  fileToArray("7.txt", 2000);
+  // for (int i = 0; i < 500; i++) {
+  //     printf("%d: %f - %d - %f - %f\n", i, pointArray[i].avgPoint, pointArray[i].frequency, pointArray[i].actualRate, pointArray[i].predictRate);
+  // }
+  fileToArray("7.txt", 200);
   actualRateRegression(pointArray, 500);
-  predictAllReview ("7.txt", 100);
+  for (int i = 0; i < 500; i++) {
+      printf("%d: %f - %d - %f - %f\n", i, pointArray[i].avgPoint, pointArray[i].frequency, pointArray[i].actualRate, pointArray[i].predictRate);
+  }
+  predictAllReview ("7.txt", 700);
   return 0;
 }
