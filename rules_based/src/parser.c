eaf5755819db31f06aba1116865b4cd551e21cd5
vincent <vincent.gbs11@gmail.com>
2019-04-24 11:09:37 -0400

    Added output file for parser

--- rules_based/src/parser.c
+++ rules_based/src/parser.c
@@ -1,6 +1,6 @@
 #include "parser.h"
 
-const char* const stopWords[] = {"ME", "MY", "MYSELF", "WE", "OUR", "OURS", "OURSELVES", "YOU", "YOUR", "YOURS",
+const char* const stopwords[] = {"ME", "MY", "MYSELF", "WE", "OUR", "OURS", "OURSELVES", "YOU", "YOUR", "YOURS",
 "YOURSELF", "YOURSELVES", "HE", "HIM", "HIS", "HIMSELF", "SHE", "HER", "HERS", "HERSELF",
 "IT", "ITS", "ITSELF", "THEY", "THEM", "THEIR", "THEIRS", "THEMSELVES", "WHAT", "WHICH",
 "WHO", "WHOM", "THIS", "THAT", "THESE", "THOSE", "AM", "IS", "ARE", "WAS", "WERE", "BE",
@@ -14,11 +14,18 @@ const char* const stopWords[] = {"ME", "MY", "MYSELF", "WE", "OUR", "OURS", "OUR
 "JUST", "SHOULD", "NOW"};
 
 // format assumed to be: 5|REVIEW TEXT
-void parseReview(char line[]) {
+void parseReview(char line[], char results[]) {
     char* str = strdup(line); // duplicate line as string
     char *p = strtok(str, "|"); // split rating from review
     char *row[MAXWORDS]; // max review of MAXWORDS words
     int i = 0, j;
+
+    FILE *output = fopen(results, "a");
+    if (output == NULL)
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
 
     while ((p != NULL) && (i < MAXWORDS))
     { // continue splitting line on spaces
@@ -28,18 +35,15 @@ void parseReview(char line[]) {
     }
 
     int stars = atoi(row[0]); // rating
-    double pos = 0; // positive DEFAULT VALUE
-    double neg = 0; // negative DEFAULT VALUE
+    double pos = 0; // positive starting value
+    double neg = 0; // negative starting value
     for (j = 1; j < i; j++) {
         struct dictRecord *word = find_record(row[j]);
         if (word) { // null check
-            // printf("[%s] in dictionary\n", row[j]);
             if (word->Positive > 0) {
-                // printf("[%s] is POSITIVE\n", row[j]);
                 pos++;
             }
             if (word->Negative > 0) {
-                // printf("[%s] is NEGATIVE\n", row[j]);
                 neg++;
             }
         }
@@ -49,22 +53,26 @@ void parseReview(char line[]) {
     }
     if (pos != 0 || neg != 0) {
         printf("This review is %f positive and %f negative, according to our analysis. The reviewer gave it %d stars. \n", (pos/i), (neg/i), stars);
+        fprintf(output, "This review is %f positive and %f negative, according to our analysis. The reviewer gave it %d stars. \n", (pos/i), (neg/i), stars);
     }
     // else {
     //     printf("There was not enough data for our algorithm.\n");
     // }
 
     free(str); // deallocate memory from line pointer
+    fclose(output);
 }
 
-void getReviews(char file[], int max) {
-    FILE* stream = fopen(file, "r"); // txt file
+void getReviews(char reviews[], char results[], int max) {
+    FILE* stream = fopen(reviews, "r"); // txt file
     char line[1024];
     int ctr = 0; // max reviews
 
+    FILE *write = fopen(results, "w");
+    fclose(write); // clear previous results, parse will append
     while (fgets(line, 1024, stream) && (ctr < max))
     {
-        parseReview(line);
+        parseReview(line, results);
         ctr++;
     }
 
